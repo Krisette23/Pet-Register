77a8439fda5f28f3dbda70230673b49fe733c817
Cristina Sanchez Lopez <s192887@student.hb.se>
2024-04-30 16:01:49 +0200

    Add project files.

---
+++ Petclass/Pet.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+
+namespace Petclass
+{
+    public class Pet
+    {
+        public string PetID { get; set; }
+        public  string PetName { get; set; }  
+        public  string PetType { get; set; }
+        public  string PetBreed { get; set; }
+        public  string PetDescription { get; set; }
+
+        public Pet()
+        {
+
+        }
+        public Pet (string petName, string petType, string petBreed)
+        {
+
+         PetName = petName;
+         PetType = petType;
+         PetBreed = petBreed;   
+
+            
+        }
+    }
+}
