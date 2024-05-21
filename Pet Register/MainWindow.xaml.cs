867f9bef9fab4262bbbd691566c1adc22d8fed60
Cristina Sanchez Lopez <s192887@student.hb.se>
2024-05-21 14:43:28 +0200

    start migrating all kod

--- Pet Register/MainWindow.xaml.cs
+++ Pet Register/MainWindow.xaml.cs
@@ -43,7 +43,7 @@ namespace Pet_Register
         }
 
         private void btnAdd_Pet_Click(object sender, RoutedEventArgs e)
-        //{
+        {
         //    pets.Add(new Pet()  );
         }
     }
