fa4e8ec9e2b144965fadab55687ffd9eca58d91d
Cristina Sanchez Lopez <s192887@student.hb.se>
2024-05-31 17:04:10 +0200

    started socket and tcp listener in server

--- Server/ConnectionHandler.cs
+++ Server/ConnectionHandler.cs
@@ -3,22 +3,43 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Net;
 using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Server
 {
-    internal class ConnectionHandler
+    public class ConnectionHandler
     {
-        BinaryReader reader;
-        BinaryWriter writer;
-        TcpClient client = new TcpClient();
+        BinaryFormatter binFormatter = new BinaryFormatter();
+        private NetworkStream networkStream;
         ConnectionHandler()
         {
-            reader = new BinaryReader(client.GetStream());
-            writer = new BinaryWriter(client.GetStream());
+            
         }
-   
+   public void Run ()
+        {
+            try
+            {
+                //create a server socket 
+                IPAddress address = Dns.GetHostAddresses(Dns.GetHostName())[0];
+                int port = 8000;
+                TcpListener serverSocket = new TcpListener(address, port); 
+                serverSocket.Start();
+                Console.WriteLine("Server started on " + address + ":" + port);
+                while (true)
+                {
+                    //accept incoming client connection
+                    TcpClient clientSocket = serverSocket.AcceptTcpClient();
+                    networkStream = clientSocket.GetStream();
+                    Console.WriteLine("Client connected");
+                    ////create a new thread for each client
+                    //Task.Run(() => HandleClient(clientSocket));
+                }
+
+            }
+        }
     }
 }
