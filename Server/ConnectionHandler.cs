b30265e6ae9225148d4182e4046f2ac47b224db1
Cristina Sanchez Lopez <s192887@student.hb.se>
2024-05-31 16:42:48 +0200

    started server implementation, not finished, just initializing the sockets and connection

--- Server/ConnectionHandler.cs
+++ Server/ConnectionHandler.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +11,14 @@ namespace Server
 {
     internal class ConnectionHandler
     {
+        BinaryReader reader;
+        BinaryWriter writer;
+        TcpClient client = new TcpClient();
+        ConnectionHandler()
+        {
+            reader = new BinaryReader(client.GetStream());
+            writer = new BinaryWriter(client.GetStream());
+        }
+   
     }
 }
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,15 +1,19 @@
-﻿using System;
+﻿using Server;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Server
+namespace PetServer
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            ConnectionHandler connectionHandler = new ConnectionHandler();
+            PetRepository petRepository = new PetRepository();
+            //connectionHandler.Run();
         }
     }
 }
